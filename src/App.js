f7abc9aae8a72c0462c9b9550aa8f40bf1b0de13
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2021-08-27 03:17:47 -0400

    F6 - saludar segun el idioma

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const form = document.querySelector("#saludador-form");
 const nombre = document.querySelector("#nombre-input");
 const edad = document.querySelector("#edad-input");
 const genero = document.querySelector("#genero-input");
+const idioma = document.querySelector("#idioma-input");
 
 // COLUMNAS: 0-> mujer ; 1 -> Hombre -> ; 2 -> otro
 // FILA: 0-> edad<15 ; 1 -> edad<30 -> ; 2 -> edad>29
@@ -14,6 +15,7 @@ const saludo_formalidad = [saludo_menor15,saludo_menor30,saludo_mayor30];
 const saludo_horario = ["Buenos dias","Buenas tardes","Buenas noches"];
 
 const saludo_completo = [saludo_horario,saludo_formalidad];
+
 
 form.addEventListener("submit",(event)=>{
     var hoy = new Date();
@@ -38,7 +40,44 @@ form.addEventListener("submit",(event)=>{
     }else if( edad.value > 29 ){
         indice_edad = 2;
     }
+
+
+    var saludo_completo;
+    if ( idioma.value == "English" ){
+        saludo_completo = ENGLISH[saludo_horario[indice_hora]]+" "+ENGLISH[saludo_formalidad[indice_edad][indice_genero] ]+" "+nombre.value; 
+    }else if (idioma.value == "Portugues"  ){
+        saludo_completo = PORTUGUESE[saludo_horario[indice_hora]]+" "+PORTUGUESE[saludo_formalidad[indice_edad][indice_genero] ]+" "+nombre.value;
+    }else if (idioma.value == "Español"){
+        saludo_completo = saludo_horario[indice_hora]+" "+ saludo_formalidad[indice_edad][indice_genero]+" "+nombre.value;
+    }
     
-    alert(saludo_horario[indice_hora]+" "+ saludo_formalidad[indice_edad][indice_genero]+" "+nombre.value);
+    alert(saludo_completo);
 } );
 
+var ENGLISH = {
+    "niña ": "girl ",
+    "niño ": "kid ",
+    "señorita ": "miss ",
+    "joven ": "young man ",
+    "señora ": "Ms ",
+    "señor ": "Mister ",
+  
+  
+    "Buenos dias": "Good morning ",
+    "Buenas tardes": " Good afternoon",
+    "Buenas noches": "Good evening"
+};
+
+var PORTUGUESE = {
+    "niña ": "garota ",
+    "niño ": "Menino ",
+    "señorita ": "senhorita ",
+    "joven ": "homem jovem ",
+    "señora ": "Em ",
+    "señor ": "Senhor ",
+  
+  
+    "Buenos dias": "Bom Dia ",
+    "Buenas tardes": " Boa tarde",
+    "Buenas noches": "Boa noite"
+  };
