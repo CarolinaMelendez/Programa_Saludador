efa623cd0fba8d84c8568840e669f41f1f8004f6
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2021-08-27 01:11:31 -0400

    F3 - saludar en base a la edad

--- src/App.js
+++ src/App.js
@@ -3,5 +3,12 @@ const nombre = document.querySelector("#nombre-input");
 const edad = document.querySelector("#edad-input");
 
 form.addEventListener("submit",(event)=>{
-    alert("Hola  " + nombre.value )
+    var edad_usuario = edad.value;
+    if(edad_usuario < 15 ){
+        alert("Hola  niñ@  " + nombre.value )
+    }else if(edad_usuario < 30){
+        alert("Hola joven  "+ nombre.value)
+    }else {
+        alert("Hola señor@  "+ nombre.value)       
+    }
 } );
