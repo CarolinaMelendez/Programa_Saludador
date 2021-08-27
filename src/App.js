575e9c76a41ec16ed9e921c7d6eeec0d3f090b5e
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2021-08-27 02:10:18 -0400

    F5 - tomando en cuenta la hora

--- src/App.js
+++ src/App.js
@@ -3,27 +3,42 @@ const nombre = document.querySelector("#nombre-input");
 const edad = document.querySelector("#edad-input");
 const genero = document.querySelector("#genero-input");
 
-// FILA: 0-> mujer ; 1 -> Hombre -> ; 2 -> otro
-// COLUMNA: 0-> edad<15 ; 1 -> edad<30 -> ; 2 -> edad>29
-const saludo_menor15 = ["Hola niña","Hola niño","Hola"];
-const saludo_menor30 = ["Hola señorita","Hola joven","Hola"];
-const saludo_mayor30 = ["Buenas señora","Buenas señor","Buenas"];
+// COLUMNAS: 0-> mujer ; 1 -> Hombre -> ; 2 -> otro
+// FILA: 0-> edad<15 ; 1 -> edad<30 -> ; 2 -> edad>29
+const saludo_menor15 = ["niña ","niño ","  "];
+const saludo_menor30 = ["señorita ","joven ","  "];
+const saludo_mayor30 = ["señora ","señor ","  "];
+
+const saludo_formalidad = [saludo_menor15,saludo_menor30,saludo_mayor30];
+
+const saludo_horario = ["Buenos dias","Buenas tardes","Buenas noches"];
+
+const saludo_completo = [saludo_horario,saludo_formalidad];
 
 form.addEventListener("submit",(event)=>{
-    var indice_saludo = 0;
+    var hoy = new Date();
+    var indice_hora = 0;
+    if( hoy.getHours()  > 12 && hoy.getHours()  < 18 ){
+        indice_hora = 1;
+    }else if( hoy.getHours()  > 18 ){
+        indice_hora = 2;
+    }
+
+
+    var indice_genero = 0;
     if( genero.value == "Hombre"){
-        indice_saludo = 1;
+        indice_genero = 1;
     }else if( genero.value == "Otro"){
-        indice_saludo = 2;
+        indice_genero = 2;
+    }
+
+    var indice_edad = 0;
+    if( edad.value > 14 && edad.value < 30){
+        indice_edad = 1;
+    }else if( edad.value > 29 ){
+        indice_edad = 2;
     }
     
-
-    if(edad.value < 15 ){
-        alert(saludo_menor15[indice_saludo]+" " + nombre.value )
-    }else if(edad.value < 30){
-        alert(saludo_menor30[indice_saludo]+" "+ nombre.value)
-    }else {
-        alert(saludo_mayor30[indice_saludo]+" " + nombre.value)       
-    }
+    alert(saludo_horario[indice_hora]+" "+ saludo_formalidad[indice_edad][indice_genero]+" "+nombre.value);
 } );
 
